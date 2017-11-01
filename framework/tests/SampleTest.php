996eecf33ee1a9f1ee699e0a611ed44a5b490aa7
developersanjog <developersanjog@gmail.com>
2017-11-01 19:51:53 +0545

    Create SampleTest.php

---
+++ framework/tests/SampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Nepframework;
+
+//Latest PHPUnit 6.4
+use PHPUnit\Framework\TestCase;
+
+class SampleTest extends TestCase
+{
+    public function testAssertTrue()
+    {
+        $this->assertTrue(TRUE);
+    }
+}
